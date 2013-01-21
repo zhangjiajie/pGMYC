bab285dbb99d81d0f3a277e9514315f55bc07469
zhangjiajie <bestzhangjiajie@gmail.com>
2013-01-21 16:29:49 +0100

    add test tree

--- Ultrametric_tree.py
+++ Ultrametric_tree.py
@@ -6,10 +6,12 @@ import operator
 from ete2 import Tree, TreeStyle, TextFace, SeqGroup
 
 class um_tree:
-	__init__(self, tree):
+	def __init__(self, tree):
 		self.tree = Tree(tree)
 	
 	
 	
 if __name__ == "__main__":
 	print("main function")
+	t = Tree("2mtree.tre")
+	t.show()
